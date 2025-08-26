83f0e7a410b9e50452f1bce17fda610652a124e2
Your Name <you@example.com>
2025-08-26 22:27:40 +0100

    fix: temporarily allow all CORS origins in backend

--- api/server.js
+++ api/server.js
@@ -80,17 +80,7 @@ const allowedOrigins = [
     'https://pharmia-chi.vercel.app'
 ];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 
 // Initial Data for Seeding
