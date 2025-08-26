8851ade2033e837c85efd2ac5a68649031c3b595
Your Name <you@example.com>
2025-08-26 22:33:32 +0100

    fix: manually set CORS headers in backend

--- api/server.js
+++ api/server.js
@@ -80,7 +80,12 @@ const allowedOrigins = [
     'https://pharmia-chi.vercel.app'
 ];
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json());
 
 // Initial Data for Seeding
