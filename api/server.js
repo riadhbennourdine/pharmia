3b8b88e787bc657b851b437caf22cd20f67418c0
Your Name <you@example.com>
2025-08-26 23:18:31 +0100

    test: further simplify backend to bare express app

--- api/server.js
+++ api/server.js
@@ -1,9 +1,6 @@
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
-
-app.use(cors()); // Allow all origins for this test
 
 app.get('/api/hello', (req, res) => {
     res.status(200).json({ message: 'Hello from Vercel Backend!' });
